97a1a2b14d198aef281865ca142fdd883b1c4df5
boshify <140970186+boshify@users.noreply.github.com>
2023-10-20 12:41:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,21 @@ def download_and_read_pdf(url):
 quality_rater_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/searchqualityevaluatorguidelines.pdf")
 helpful_content_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/Google%20Helpful%20Content%20Guidelines.pdf")
 
+def download_and_read_pdf(url):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with BytesIO(response.content) as open_pdf_file:
+        reader = PyPDF2.PdfReader(open_pdf_file)
+        content = ""
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            content += page.extract_text()
+    return content
+
+quality_rater_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/searchqualityevaluatorguidelines.pdf")
+helpful_content_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/Google%20Helpful%20Content%20Guidelines.pdf")
+
 def get_body_text(url):
     headers = {
         'User-Agent': 'Mozilla/5.0'
@@ -63,8 +78,28 @@ def analyze_page(body_text, guideline_type):
     
     return response.choices[0].message['content'].strip()
 
+def analyze_question(body_text, guideline_type, question):
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+    if guideline_type == "Quality Raters":
+        guideline_content = quality_rater_guidelines
+    else:
+        guideline_content = helpful_content_guidelines
+
+    prompt = (f"Please start by reading this document: {guideline_content}\n\n"
+              f"Next, read my article here: {body_text[:3000]}\n\n"
+              f"Based strictly on the guidelines or the principals outlined in the first document, analyze this aspect:\n\n"
+              f"**{question}**\n\n")
     
-    return response.choices[0].text.strip()
+    # Adjust the call to the OpenAI API to use the chat endpoint
+    messages = [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}]
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=messages
+    )
+    
+    return response.choices[0].message['content'].strip()
 
 def main():
     st.title('Google Quality Rater & Helpful Content Guideline Checker App')
@@ -79,15 +114,20 @@ def main():
         progress_bar.progress(50)
         
         if body_text:
-            st.write("Analysis based on Google Quality Raters Guidelines:")
-            quality_raters_analysis = analyze_page(body_text, "Quality Raters")
-            st.markdown(quality_raters_analysis)
+            if st.checkbox("Comprehensive Web Page Quality Review Checklist"):
+                checklist_questions = [
+                    "Determine the primary purpose of the page.",
+                    "Ensure the purpose serves visitors' genuine interests.",
+                    "Verify the page provides beneficial content aligned with its purpose.",
+                    # Add all other questions from the checklist here
+                ]
 
-            st.write("Analysis based on Google Helpful Content Guidelines:")
-            helpful_content_analysis = analyze_page(body_text, "Helpful Content")
-            st.markdown(helpful_content_analysis)
-
-            progress_bar.progress(100)
+                for question in checklist_questions:
+                    st.write(f"Analysis for: {question}")
+                    analysis = analyze_question(body_text, "Quality Raters", question)
+                    st.markdown(analysis)
+                    
+                progress_bar.progress(100)
 
 if __name__ == '__main__':
     main()
