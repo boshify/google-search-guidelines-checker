80e93627bb6d4a62f431dfe69de9f7ae08cfca11
boshify <140970186+boshify@users.noreply.github.com>
2023-10-20 12:45:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,38 +2,7 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import openai
-import PyPDF2
 from io import BytesIO
-
-def download_and_read_pdf(url):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with BytesIO(response.content) as open_pdf_file:
-        reader = PyPDF2.PdfReader(open_pdf_file)
-        content = ""
-        for page_num in range(len(reader.pages)):
-            page = reader.pages[page_num]
-            content += page.extract_text()
-    return content
-
-quality_rater_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/searchqualityevaluatorguidelines.pdf")
-helpful_content_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/Google%20Helpful%20Content%20Guidelines.pdf")
-
-def download_and_read_pdf(url):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with BytesIO(response.content) as open_pdf_file:
-        reader = PyPDF2.PdfReader(open_pdf_file)
-        content = ""
-        for page_num in range(len(reader.pages)):
-            page = reader.pages[page_num]
-            content += page.extract_text()
-    return content
-
-quality_rater_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/searchqualityevaluatorguidelines.pdf")
-helpful_content_guidelines = download_and_read_pdf("https://github.com/boshify/google-search-guidelines-checker/raw/main/Google%20Helpful%20Content%20Guidelines.pdf")
 
 def get_body_text(url):
     headers = {
@@ -48,37 +17,7 @@ def get_body_text(url):
         st.warning(f"Failed to crawl {url}. Error: {e}")
         return None
 
-def analyze_page(body_text, guideline_type):
-    openai.api_key = st.secrets["OPENAI_API_KEY"]
-    
-    if guideline_type == "Quality Raters":
-        guideline_content = quality_rater_guidelines
-    else:
-        guideline_content = helpful_content_guidelines
-    
-    prompt = (f"Please start by reading this document: {guideline_content}\n\n"
-              f"Next, read my article here: {body_text[:3000]}\n\n")
-    
-    if guideline_type == "Quality Raters":
-        prompt += (f"Based strictly on the guidelines or the principals outlined in the first document, analyze this article in terms of the depth and detail of the content, "
-                   f"the demonstration of expertise and credibility, and how well it fulfills the user's intent. Provide a list of specific action points for potential improvements. "
-                   f"Please exclude any generic SEO advice. Output your response in markdown format.")
-    else:
-        prompt += (f"Based strictly on the guidelines or the principals outlined in the first document, analyze this article in terms of the relevance, clarity, and utility of its content. "
-                   f"Identify areas where the content can be made more helpful for users. Provide specific action points for potential improvements. "
-                   f"Please exclude any generic SEO advice. Output your response in markdown format.")
-    
-    # Adjust the call to the OpenAI API to use the chat endpoint
-    messages = [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}]
-    
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=messages
-    )
-    
-    return response.choices[0].message['content'].strip()
-
-def analyze_question(body_text, guideline_type, question):
+def analyze_questions(body_text, guideline_type, questions):
     openai.api_key = st.secrets["OPENAI_API_KEY"]
 
     if guideline_type == "Quality Raters":
@@ -88,8 +27,10 @@ def analyze_question(body_text, guideline_type, question):
 
     prompt = (f"Please start by reading this document: {guideline_content}\n\n"
               f"Next, read my article here: {body_text[:3000]}\n\n"
-              f"Based strictly on the guidelines or the principals outlined in the first document, analyze this aspect:\n\n"
-              f"**{question}**\n\n")
+              f"Based strictly on the guidelines or the principals outlined in the first document, analyze the following aspects:\n\n")
+    
+    for question in questions:
+        prompt += f"**{question}**\n\n"
     
     # Adjust the call to the OpenAI API to use the chat endpoint
     messages = [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}]
@@ -105,8 +46,9 @@ def main():
     st.title('Google Quality Rater & Helpful Content Guideline Checker App')
 
     url = st.text_input('Enter the URL of the page you want to analyze:')
+    run_analysis = st.button("Get Report")
     
-    if url:
+    if run_analysis:
         progress_bar = st.progress(0)
         st.write("Fetching and analyzing page content...")
 
@@ -114,20 +56,64 @@ def main():
         progress_bar.progress(50)
         
         if body_text:
-            if st.checkbox("Comprehensive Web Page Quality Review Checklist"):
-                checklist_questions = [
-                    "Determine the primary purpose of the page.",
-                    "Ensure the purpose serves visitors' genuine interests.",
-                    "Verify the page provides beneficial content aligned with its purpose.",
-                    # Add all other questions from the checklist here
-                ]
+            st.write("Analysis based on Google Quality Raters Guidelines:")
+            analysis = analyze_questions(body_text, "Quality Raters", quality_rater_questions)
+            st.markdown(analysis)
 
-                for question in checklist_questions:
-                    st.write(f"Analysis for: {question}")
-                    analysis = analyze_question(body_text, "Quality Raters", question)
-                    st.markdown(analysis)
-                    
-                progress_bar.progress(100)
+            progress_bar.progress(100)
 
 if __name__ == '__main__':
+    quality_rater_guidelines = """Your Quality Rater Guidelines content here"""
+
+    quality_rater_questions = [
+        "Determine the primary purpose of the page.",
+        "Ensure the purpose serves visitors' genuine interests.",
+        "Verify the page provides beneficial content aligned with its purpose.",
+        "Distinguish Main Content (MC), Supplementary Content (SC), and Advertisements (Ads).",
+        "Assess the quality and relevance of Main Content.",
+        "Ensure Supplementary Content enhances user experience without detracting from Main Content.",
+        "Check that Advertisements are clearly marked and unobtrusive.",
+        "Determine if the page contains YMYL information (related to health, finance, safety, or well-being).",
+        "Ensure YMYL content is accurate, trustworthy, and backed by expertise (E-A-T).",
+        "Evaluate the expertise, authoritativeness, and trustworthiness of content creators.",
+        "Confirm the content creator possesses deep knowledge of the subject matter.",
+        "Identify who is responsible for the website.",
+        "Verify authorship information is present.",
+        "Include contact details if applicable.",
+        "Assess the website and content creator's reputation.",
+        "Consider user reviews, expert recommendations, news articles, and awards.",
+        "Verify expertise through real-life experiences.",
+        "Ensure the main heading or page title provides an accurate summary of the content.",
+        "Avoid exaggeration or sensationalism in headings or titles.",
+        "Does the content genuinely serve visitors' interests?",
+        "Is the content produced for quality or primarily for search engine rankings?",
+        "Does excessive automation impact content quality?",
+        "Does the content add substantial value or merely summarize others' work?",
+        "Is the content created based on trends or tailored to the audience's needs?",
+        "Does the content leave users feeling like they need to search again to get better information?",
+        "Does your content actually answer the question it promised to answer?",
+        "Will someone reading your content leave feeling like they've had a satisfying experience and learned enough about a topic to help achieve their goal?",
+        "Does the content provide substantial value when compared to other pages in search results?",
+        "Does the content provide original information, reporting, research, or analysis?",
+        "Does the content provide a substantial, complete, or comprehensive description of the topic?",
+        "Does the content provide insightful analysis or interesting information that is beyond the obvious?",
+        "If the content draws on other sources, does it avoid simply copying or rewriting those sources, and instead provide substantial additional value and originality?",
+        "Does the main heading or page title provide a descriptive, helpful summary of the content?",
+        "Does the main heading or page title avoid exaggeration or being shocking in nature?",
+        "Is this the sort of page you'd want to bookmark, share with a friend, or recommend?",
+        "Would you expect to see this content in or referenced by a printed magazine, encyclopedia, or book?",
+        "Is it self-evident to your visitors who authored your content, do pages carry a byline, where one might be expected?",
+        "Do bylines lead to further information about the author or authors involved, giving background about them and the areas they write about?",
+        "If automation is used to substantially generate content, Is the use of automation, including AI-generation, self-evident to visitors through disclosures or in other ways?",
+        "Are you providing background about how automation or AI-generation was used to create content (in case it was)?",
+        "Are you explaining why automation or AI was seen as useful to produce content (in case it was)?",
+        "Does the content present information in a way that makes you want to trust it, such as clear sourcing, evidence of the expertise involved, background about the author or the site that publishes it, such as through links to an author page or a site's About page?",
+        "If someone researched the site producing the content, would they come away with an impression that it is well-trusted or widely-recognized as an authority on its topic?",
+        "Is this content written by an expert or enthusiast who demonstrably knows the topic well?",
+        "Is the content free from easily-verified factual errors?",
+        "Does your content clearly demonstrate first-hand expertise and a depth of knowledge (for example, expertise that comes from having actually used a product or service, or visiting a place)?",
+        "Is the content free from spelling or stylistic issues?",
+        "Was the content produced well, or does it appear sloppy or hastily produced?"
+    ]
+
     main()
