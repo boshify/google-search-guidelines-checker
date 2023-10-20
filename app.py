28cb69cb76160fce6d5f2b1d31af8e92c3a558ff
boshify <140970186+boshify@users.noreply.github.com>
2023-10-20 11:49:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ def get_body_text(url):
 
 def get_recommendations(body_text, guideline_type):
     openai.api_key = st.secrets["OPENAI_API_KEY"]
-    # The prompt remains largely the same, with slight variations based on guideline type
     prompt = (f"Check if this page meets Google {guideline_type} Guidelines. [... similar conditions ...] "
               f"Page: {body_text[:3000]}")
     
@@ -31,25 +30,20 @@ def get_recommendations(body_text, guideline_type):
 def main():
     st.title('Google Quality Rater & Helpful Content Guideline Checker App')
     
-    guideline_type = st.selectbox("Select the guideline type:", ["Quality Raters", "Helpful Content"])
-    
-    url1 = st.text_input("Enter the URL of the first page you want to analyze:")
-    url2 = st.text_input("Enter the URL of the second page you want to analyze:")
+    url = st.text_input("Enter the URL of the page you want to analyze:")
     
     if st.button('Analyze'):
         with st.spinner('Analyzing...'):
-            # Analyze the first URL
-            body_text1 = get_body_text(url1)
-            if body_text1:
-                recommendations1 = get_recommendations(body_text1, guideline_type)
-                st.markdown(f"**Recommendations for {url1}**")
+            body_text = get_body_text(url)
+            if body_text:
+                # Analyze for Quality Raters
+                recommendations1 = get_recommendations(body_text, "Quality Raters")
+                st.markdown(f"**Recommendations based on Quality Raters Guidelines for {url}**")
                 st.markdown(f'<div style="border:2px solid #F0F2F6; padding:10px; border-radius:10px; margin-bottom: 10px;">{recommendations1}</div>', unsafe_allow_html=True)
-            
-            # Analyze the second URL
-            body_text2 = get_body_text(url2)
-            if body_text2:
-                recommendations2 = get_recommendations(body_text2, guideline_type)
-                st.markdown(f"**Recommendations for {url2}**")
+                
+                # Analyze for Helpful Content
+                recommendations2 = get_recommendations(body_text, "Helpful Content")
+                st.markdown(f"**Recommendations based on Helpful Content Guidelines for {url}**")
                 st.markdown(f'<div style="border:2px solid #F0F2F6; padding:10px; border-radius:10px; margin-bottom: 10px;">{recommendations2}</div>', unsafe_allow_html=True)
         
     st.markdown("---")
