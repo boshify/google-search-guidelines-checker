02423fcfd0ccf7bc630c5547e0c48f907c32e0d6
boshify <140970186+boshify@users.noreply.github.com>
2023-10-20 11:47:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup
 import openai
 
 def get_body_text(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0'
-    }
+    headers = {'User-Agent': 'Mozilla/5.0'}
     try:
         response = requests.get(url, headers=headers, timeout=10)
         response.raise_for_status()
@@ -18,44 +16,42 @@ def get_body_text(url):
 
 def get_recommendations(body_text, guideline_type):
     openai.api_key = st.secrets["OPENAI_API_KEY"]
+    # The prompt remains largely the same, with slight variations based on guideline type
+    prompt = (f"Check if this page meets Google {guideline_type} Guidelines. [... similar conditions ...] "
+              f"Page: {body_text[:3000]}")
     
-    # Use the appropriate prompt for the selected guideline type
-    if guideline_type == "Quality Raters":
-        prompt = (f"Check if this page meets Google Quality Raters Guidelines. You must Give specific and actionable examples for how to further improve it."
-                  f"Do not make a recommendation if you can not provide a specific example. Recommendations should provide specific examples of text on the page."
-                  f"Please be very scrutinizing. You should only pass a page if it is exceptional. Even if you pass a page, you must further improve it."
-                  f"You must exclude recommendations for images or links. State that you can not crawl these aspects yet. Do not assume the page does not have images or links as you can not detect them."
-                  f"If the page meets guidelines, provide specific examples of how it could be further improved. Use line breaks and spacing to make output easy to read. Content can always be improved."
-                  f"Add a disclaimer: Mention your limitations that you only crawl the text of the page and that it's ultimately up to the user to determine if the page meets guidelines."
-                  f"Markdown format: Your response output must be in markdown format. Using headings and sub headings, bold, lists, and line breaks. This improves reading clarity."
-                  f"Page: {body_text[:3000]}")  # Truncate to avoid hitting API limits
-    else:
-        prompt = (f"Check if this page meets Google Helpful Content Guidelines. You must Give specific and actionable examples for how to further improve it."
-                  f"Do not make a recommendation if you can not provide a specific example. Recommendations should provide specific examples of text on the page."
-                  f"Please be very scrutinizing. You should only pass a page if it is exceptional. Even if you pass a page, you must further improve it."
-                  f"You must exclude recommendations for images or links. State that you can not crawl these aspects yet. Do not assume the page does not have images or links as you can not detect them."
-                  f"If the page meets guidelines, provide specific examples of how it could be further improved. Use line breaks and spacing to make output easy to read. Content can always be improved."
-                  f"Add a disclaimer: Mention your limitations that you only crawl the text of the page and that it's ultimately up to the user to determine if the page meets guidelines."
-                  f"Markdown format: Your response output must be in markdown format. Using headings and sub headings, bold, lists, and line breaks. This improves reading clarity."
-                  f"Page: {body_text[:3000]}")  # Truncate to avoid hitting API limits
-
     response = openai.Completion.create(
         model="gpt-3.5-turbo-16k",
         prompt=prompt,
         max_tokens=16385
     )
-
+    
     return response.choices[0].text.strip()
 
 def main():
     st.title('Google Quality Rater & Helpful Content Guideline Checker App')
+    
     guideline_type = st.selectbox("Select the guideline type:", ["Quality Raters", "Helpful Content"])
-    url = st.text_input("Enter the URL of the page you want to analyze:")
+    
+    url1 = st.text_input("Enter the URL of the first page you want to analyze:")
+    url2 = st.text_input("Enter the URL of the second page you want to analyze:")
+    
     if st.button('Analyze'):
-        body_text = get_body_text(url)
-        if body_text:
-            recommendations = get_recommendations(body_text, guideline_type)
-            st.markdown(recommendations)
+        with st.spinner('Analyzing...'):
+            # Analyze the first URL
+            body_text1 = get_body_text(url1)
+            if body_text1:
+                recommendations1 = get_recommendations(body_text1, guideline_type)
+                st.markdown(f"**Recommendations for {url1}**")
+                st.markdown(f'<div style="border:2px solid #F0F2F6; padding:10px; border-radius:10px; margin-bottom: 10px;">{recommendations1}</div>', unsafe_allow_html=True)
+            
+            # Analyze the second URL
+            body_text2 = get_body_text(url2)
+            if body_text2:
+                recommendations2 = get_recommendations(body_text2, guideline_type)
+                st.markdown(f"**Recommendations for {url2}**")
+                st.markdown(f'<div style="border:2px solid #F0F2F6; padding:10px; border-radius:10px; margin-bottom: 10px;">{recommendations2}</div>', unsafe_allow_html=True)
+        
     st.markdown("---")
     st.markdown('Made by [YourName.com](https://yourwebsite.com)')
 
