7fd00bc4b3ea899a4fadceddf03dfa25e5653149
boshify <140970186+boshify@users.noreply.github.com>
2023-10-20 11:53:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from bs4 import BeautifulSoup
 import openai
 
 def get_body_text(url):
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
     try:
         response = requests.get(url, headers=headers, timeout=10)
         response.raise_for_status()
@@ -16,38 +18,36 @@ def get_body_text(url):
 
 def get_recommendations(body_text, guideline_type):
     openai.api_key = st.secrets["OPENAI_API_KEY"]
-    prompt = (f"Check if this page meets Google {guideline_type} Guidelines. [... similar conditions ...] "
-              f"Page: {body_text[:3000]}")
+    messages = [
+        {"role": "system", "content": "You are a useful SEO assistant. Exclude conclusions from your output."},
+        {"role": "user", "content": f"Check if this page meets Google {guideline_type} Guidelines. You must Give specific and actionable examples for how to further improve it."
+                                    f"Do not make a recommendation if you can not provide a specific example. Recommendations should provide specific examples of text on the page."
+                                    f"Please be very scrutinizing. You should only pass a page if it is exceptional. Even if you pass a page, you must further improve it."
+                                    f"Do not assume the page does not have images or links as you may not be able to detect them."
+                                    f"If the page meets guidelines, provide specific examples of how it could be further improved. Use line breaks and spacing to make output easy to read. Content can always be improved."
+                                    f"Add a disclaimer: Mention your limitations and that it's ultimately up to the user to determine if the page meets guidelines."
+                                    f"Markdown format: Your response output must be in markdown format. Using headings and subheadings, bold, lists, and line breaks. This improves reading clarity."
+                                    f"Page: {body_text[:3000]}"}
+    ]
     
-    response = openai.Completion.create(
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
-        prompt=prompt,
-        max_tokens=16385
+        messages=messages
     )
     
-    return response.choices[0].text.strip()
+    return response.choices[0].message['content'].strip()
 
 def main():
     st.title('Google Quality Rater & Helpful Content Guideline Checker App')
-    
+    guideline_type = st.selectbox("Select the guideline type:", ["Quality Raters", "Helpful Content"])
     url = st.text_input("Enter the URL of the page you want to analyze:")
-    
     if st.button('Analyze'):
-        with st.spinner('Analyzing...'):
-            body_text = get_body_text(url)
-            if body_text:
-                # Analyze for Quality Raters
-                recommendations1 = get_recommendations(body_text, "Quality Raters")
-                st.markdown(f"**Recommendations based on Quality Raters Guidelines for {url}**")
-                st.markdown(f'<div style="border:2px solid #F0F2F6; padding:10px; border-radius:10px; margin-bottom: 10px;">{recommendations1}</div>', unsafe_allow_html=True)
-                
-                # Analyze for Helpful Content
-                recommendations2 = get_recommendations(body_text, "Helpful Content")
-                st.markdown(f"**Recommendations based on Helpful Content Guidelines for {url}**")
-                st.markdown(f'<div style="border:2px solid #F0F2F6; padding:10px; border-radius:10px; margin-bottom: 10px;">{recommendations2}</div>', unsafe_allow_html=True)
-        
+        body_text = get_body_text(url)
+        if body_text:
+            recommendations = get_recommendations(body_text, guideline_type)
+            st.markdown(recommendations)
     st.markdown("---")
-    st.markdown('Made by [YourName.com](https://yourwebsite.com)')
+    st.markdown('Made by [JonathanBoshoff.com](https://jonathanboshoff.com)')
 
 if __name__ == "__main__":
     main()
